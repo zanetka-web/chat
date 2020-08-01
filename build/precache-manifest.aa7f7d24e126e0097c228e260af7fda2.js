d6ee49130a9f61165de7361ecfec5fe8c14de9d8
Zaneta Pereira <zanetka281091@gmail.com>
2020-08-01 10:20:31 +0200

    adding build

---
+++ build/precache-manifest.aa7f7d24e126e0097c228e260af7fda2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e932a2a874c69ea698d88b89a4a86b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37053942e11f82d79410",
+    "url": "/static/css/main.7a9e9f90.chunk.css"
+  },
+  {
+    "revision": "5be4f452c9698981f12a",
+    "url": "/static/js/2.4834b79e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4834b79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37053942e11f82d79410",
+    "url": "/static/js/main.8d2f236e.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
